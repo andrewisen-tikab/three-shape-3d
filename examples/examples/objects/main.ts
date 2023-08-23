9679bda9882fcf0ba12026587f2ad5eeb35cd8c0
André "MG" Wisén <andre.wisen@gmail.com>
2023-08-23 21:34:16 +0200

    Cleanup

--- examples/examples/objects/main.ts
+++ examples/examples/objects/main.ts
@@ -10,34 +10,62 @@ const { gltflLoader, selector, transformShapeControls } = example;
 
 let modelWidth: number;
 
+// We will use the selected object as the shape to place objects on.
 const selectedShape3D = selector.getSelectedShape();
 if (!selectedShape3D) throw new Error('No selected object');
 
+/**
+ * The model is loaded asynchronously, so we need to keep a reference to it.
+ */
 let model: THREE.Object3D;
+/**
+ * We will add the objects to a group, so we can easily remove them.
+ */
 const modelGroup = new THREE.Group();
 selectedShape3D.add(modelGroup);
 
+// A custom factory to create the objects on the shape.
 const objectsOnShapeFactory = new ObjectsOnShapeFactory();
 objectsOnShapeFactory.setPoolPosition([0, -1_000_000, 0]);
 
+/**
+ * Handle the creation of the objects on the shape.
+ */
 const addObjectsOnShape = () => {
     beginPool();
     adjustPool();
     // endPool();
 };
 
+/**
+ * Called when **before (!)** the user starts to drag the shape.
+ */
 const beginPool = () => {
-    objectsOnShapeFactory.beginPool(modelGroup, model, selectedShape3D, { width: modelWidth });
+    objectsOnShapeFactory.preparePool(modelGroup, model, selectedShape3D, { width: modelWidth });
 };
 
+/**
+ * Called when the user drags the shape.
+ */
 const adjustPool = () => {
-    objectsOnShapeFactory.adjustPool(selectedShape3D, { width: modelWidth });
+    objectsOnShapeFactory.adjustPoolMatrices(selectedShape3D, { width: modelWidth });
 };
 
+/**
+ * Called when the user stops dragging the shape.
+ */
 const endPool = () => {
     objectsOnShapeFactory.endPool();
 };
 
+// Setup event listeners.
+// selectedObject.addEventListener('vertex-updated', addObjectsOnShape);
+transformShapeControls.addEventListener('mouseDown', beginPool);
+transformShapeControls.addEventListener('vertexChange', adjustPool);
+
+transformShapeControls.addEventListener('mouseUp', endPool);
+
+// Finally, load the model.
 gltflLoader.load('../../../metal-fence.glb', (gltf) => {
     model = gltf.scene;
 
@@ -46,9 +74,3 @@ gltflLoader.load('../../../metal-fence.glb', (gltf) => {
 
     addObjectsOnShape();
 });
-
-// selectedObject.addEventListener('vertex-updated', addObjectsOnShape);
-transformShapeControls.addEventListener('mouseDown', beginPool);
-transformShapeControls.addEventListener('vertexChange', adjustPool);
-
-transformShapeControls.addEventListener('mouseUp', endPool);
